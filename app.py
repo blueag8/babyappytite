74c20bfc327e5194a280cfc34cfadeb0b9ddd315
Ubuntu <ubuntu@ip-172-31-93-110.ec2.internal>
2019-08-06 11:10:55 +0000

    updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import pymongo
 
 from bson.objectid import ObjectId
 from flask import Flask, render_template, redirect, request, url_for
@@ -11,9 +10,30 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] ="Baby_Recipes"
 app.config['MONGODB_URI'] = os.getenv("MONGO_URI")
 
+mongo = PyMongo(app)
+#home page
 @app.route('/')
-def Hello():
-    return('Hello')
+def index():
+    return render_template("index.html")
+    
+#get recipes
+
+@app.route('/sixmonth_recipes')
+def sixmonth_recipes():
+    recipes=mongo.db.recipes.find_one_or_404({"category_age":"6 months +"})
+    return render_template("sixmonth_recipes.html", recipes=recipes)
+
+@app.route('/sevenmonth_recipes')
+def sevenmonth_recipes():
+    return render_template("sevenmonth_recipes.html", recipes=mongo.db.recipes.find({"category_age":"7 months +"}))
+
+@app.route('/tenmonth_recipes')
+def tenmonth_recipes():
+    return render_template("tenmonth_recipes.html", recipes=mongo.db.recipes.find({"category_age":"10 months +"}))
+    
+@app.route('/twelvemonth_recipes')
+def twelvemonth_recipes():
+    return render_template("twelvemonth_recipes.html",recipes=mongo.db.recipes.find({"category_age":"12 months +"}))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
