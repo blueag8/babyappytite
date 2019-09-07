d235232ea908202d7ec99707dd14a9d469c19310
Ubuntu <ubuntu@ip-172-31-93-110.ec2.internal>
2019-09-07 21:33:09 +0000

    set debug to false

--- app.py
+++ app.py
@@ -164,4 +164,4 @@ def stars(recipe_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
